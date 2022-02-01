61cc675134c0634312cfc923cf2641bc0b5cd71a
Gus Teran <ateran@wpi.edu>
2022-02-01 16:14:17 -0500

    Add runtime comment to h

--- src/main/java/h/HappyAndFamousOneJob.java
+++ src/main/java/h/HappyAndFamousOneJob.java
@@ -98,6 +98,15 @@ public class HappyAndFamousOneJob {
         }
     }
 
+    /**
+     * Use run conditions in intellij to pass the files needed
+     * For example my args are:
+     * file:///C:/Users/Gus/Documents/Code/CS-4433/Are-you-My-Friend-Analytics/DataOutput/friends.csv
+     * file:///C:/Users/Gus/Documents/Code/CS-4433/Are-you-My-Friend-Analytics/DataOutput/myPage.csv
+     * file:///C:/Users/Gus/Documents/Code/CS-4433/Project1/output/h.txt
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         long startTime = System.currentTimeMillis();
 
